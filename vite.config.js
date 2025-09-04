a8004ac5207f418e1648442e9b0be076c96e2319
Mhmd-Elawady <mhmdelmetwally22@gmail.com>
2025-09-04 19:08:11 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
-  base: '/School-Education/', 
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom']
-  }
+  resolve: { dedupe: ["react", "react-dom"] },
 });
